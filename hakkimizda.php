dda02f1fa25b286f38534d442284884f51be4008
Sude Korkmaz <135531605+CSudeKorkmaz@users.noreply.github.com>
2023-06-04 18:40:41 +0300

    Add files via upload

---
+++ hakkimizda.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>AnaSayfa | Kurumsal</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Oxygen&display=swap"
+      rel="stylesheet"
+    />
+    <script
+      src="https://kit.fontawesome.com/60f65977b7.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="owl/owl.carousel.min.css" />
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css" />
+  </head>
+  <body>
+    <section id="menu">
+      <div id="logo">Kurumsal</div>
+      <nav>
+        <a href="#menu"><i class="fas fa-home ikon"></i> Anasayfa</a>
+        <a href="#hakkimizda"><i class="fas fa-circle-info ikon"></i> Hakkımızda</a>
+        <a href="#urunler"><i class="fas fa-angles-right ikon"></i> Ürün/Hizmet</a>
+        <a href="#referans"><i class="fas fa-users ikon"></i> Referanslar</a>
+        <a href="#iletisim"><i class="fas fa-address-book ikon"></i> İletişim</a>
+      </nav>
+    </section>
+    <main>
+          <h1>HAKKIMIZDA</h1>
+          <img id="hakkimizdaRSM" src="hakkimizdda.png" alt="" />
+          <h3>Biz kimiz?</h3>
+          <p class="hakYazi">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="hakYazi">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <h3>Kurumsal</h3>
+          <p class="hakYazi">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="hakYazi">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+          <hr />
+          <h3>Vizyon & Misyon</h3>
+          <p class="hakYazi">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+        </main>
---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>AnaSayfa | Kurumsal</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Oxygen&display=swap"
+      rel="stylesheet"
+    />
+    <script
+      src="https://kit.fontawesome.com/60f65977b7.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="owl/owl.carousel.min.css" />
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css" />
+  </head>
+  <body >
+    <section id="menu">
+      <div id="logo">Kurumsal</div>
+      <nav>
+        <a href="#menu"><i class="fas fa-home ikon"></i> Anasayfa</a>
+        <a href="#hakkimizda"><i class="fas fa-circle-info ikon"></i> Hakkımızda</a>
+        <a href="#urunler"><i class="fas fa-angles-right ikon"></i> Ürün/Hizmet</a>
+        <a href="#referans"><i class="fas fa-users ikon"></i> Referanslar</a>
+        <a href="#iletisim"><i class="fas fa-address-book ikon"></i> İletişim</a>
+      </nav>
+    </section>
+    <section id="anasayfa">
+      <div id="black"></div>
+      <div id="icerik">
+        <h2>Kurumsal</h2>
+        <hr width="300" align="left" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vel
+          aliquid sequi asperiores veniam delectus nisi, minima suscipit
+          officia! Minima ipsum rerum iure magnam. Consectetur quaerat et
+          corporis asperiores accusantium.
+        </p>
+      </div>
+    </section>
+    <section id="hakkimizda">
+      <h3>Hakkımızda</h3>
+
+      <div class="container">
+        <div id="sol">
+          <h5 id="h5sol">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Voluptatibus impedit
+          </h5>
+        </div>
+        <div id="sag">
+          <span>L</span>
+          <p id="psag">
+            orem ipsum dolor sit amet consectetur adipisicing elit. Alias nisi
+            adipisci, autem quidem labore voluptatibus sapiente nulla rerum
+            consequatur. Numquam reprehenderit id velit perspiciatis facere a
+            dignissimos quibusdam mollitia obcaecati!
+          </p>
+        </div>
+        <img
+          src="New_York_Midtown_Skyline_at_night_-_Jan_2006_edit1.jpg"
+          alt=""
+          class="img-fluid mt100 blog-content"
+        />
+        <p id="pson">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse debitis
+          corporis cumque reprehenderit delectus doloremque magnam illum
+          deleniti enim. Possimus, architecto aliquid. Consequuntur reiciendis
+          necessitatibus cum quasi voluptates facilis impedit!
+        </p>
+      </div>
+    </section>
+    <section id="urunler">
+      <div class="container">
+        <h3>Ürünler ve Hizmetler</h3>
+        <div class="owl-carousel owl-theme">
+          <div class="card item" data-merge="1.5">
+            <img
+              src="Latest-Web-Design-Trends-and-Standards.jpg"
+              alt=""
+              class="img-fluid blog-content"
+            />
+            <h5 class="baslikcard">Ürün1</h5>
+            <p class="cardp">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestias aliquam qui quidem
+            </p>
+          </div>
+          <div class="card item" data-merge="1.5">
+            <img
+              src="Latest-Web-Design-Trends-and-Standards.jpg"
+              alt=""
+              class="img-fluid blog-content"
+            />
+            <h5 class="baslikcard">Ürün1</h5>
+            <p class="cardp">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestias aliquam qui quidem
+            </p>
+          </div>
+          <div class="card item" data-merge="1.5">
+            <img
+              src="Latest-Web-Design-Trends-and-Standards.jpg"
+              alt=""
+              class="img-fluid blog-content"
+            />
+            <h5 class="baslikcard">Ürün1</h5>
+            <p class="cardp">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestias aliquam qui quidem
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="referans">
+      <div class="container">
+        <h3 id="referh3">Referanslar</h3>
+        <div class="sutun-sol-sag">
+          <img
+            src="yapay-zeka-sahte-yuz4.jpeg"
+            alt=""
+            class="img-fluid oval blog-content"
+          />
+          <h4 class="refisim">ColodinWare</h4>
+          <p class="refp">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            deserunt?
+          </p>
+          <div class="ref-ikon">
+            <a href="#"><i class="fab fa-facebook social"></i></a>
+            <a href="#"><i class="fab fa-instagram social"></i></a>
+            <a href="#"><i class="fab fa-twitter social"></i></a>
+          </div>
+        </div>
+
+        <div class="sutun">
+          <img src="foto1(1).jpg" alt="" class="img-fluid oval blog-content" />
+          <h4 class="refisim">Slytia Web Design</h4>
+          <p class="refp">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            deserunt?
+          </p>
+          <div class="ref-ikon">
+            <a href="#"><i class="fab fa-facebook social"></i></a>
+            <a href="#"><i class="fab fa-instagram social"></i></a>
+            <a href="#"><i class="fab fa-twitter social"></i></a>
+          </div>
+        </div>
+
+        <div class="sutun-sol-sag">
+          <img src="her-tiklamada.jpg" alt="" class="img-fluid oval blog-content" />
+          <h4 class="refisim">Samba SEO</h4>
+          <p class="refp">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+            deserunt?
+          </p>
+          <div class="ref-ikon">
+            <a href="#"><i class="fab fa-facebook social"></i></a>
+            <a href="#"><i class="fab fa-instagram social"></i></a>
+            <a href="#"><i class="fab fa-twitter social"></i></a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="iletisim">
+      <div class="container">
+        <h3 id="h3iletisim">İletişim</h3>
+        <form action="index.php" method="post">
+          <div id="iletisimopak">
+            <div id="formgroup">
+              <div id="solform">
+                <input
+                  type="text"
+                  name="isim"
+                  placeholder="Ad Soyad"
+                  required
+                  class="form-control"
+                />
+                <input
+                  type="text"
+                  name="tel"
+                  placeholder="Telefon numarası"
+                  required
+                  class="form-control"
+                />
+              </div>
+              <div id="sagform">
+                <input
+                  type="email"
+                  name="mail"
+                  placeholder="email Adresiniz"
+                  required
+                  class="form-control"
+                />
+                <input
+                  type="text"
+                  name="konu"
+                  placeholder="Konu başlığı"
+                  required
+                  class="form-control"
+                />
+              </div>
+
+              <textarea
+                name="mesaj"
+                id=""
+                cols="30"
+                placeholder="Mesaj Giriniz"
+                rows="10"
+                required
+                class="form-control"
+              ></textarea>
+              <input type="submit" value="Gönder" />
+            </div>
+
+            <div id="adres">
+              <h4 id="adresbaslik">Adres :</h4>
+              <p class="adresp">Molla Yusuf mahallesi</p>
+              <p class="adresp">Akyaka Caddesi</p>
+              <p class="adresp">Oğuzbey Sokak no:123</p>
+              <p class="adresp">0242 243 44 45</p>
+              <p class="adresp">Email : kurumsalwebsite@tasarımı.com</p>
+            </div>
+          </div>
+        </form>
+
+        <footer>
+          <div id="copyright">2023 | Tüm Hakları Saklıdır</div>
+          <div id="socialfooter">
+            <a href="#"><i class="fab fa-facebook social"></i></a>
+            <a href="#"><i class="fab fa-instagram social"></i></a>
+            <a href="#"><i class="fab fa-twitter social"></i></a>
+          </div>
+          <a href="#menu"><i class="fas fa-angle-up" id="up"></i></a>
+        </footer>
+      </div>
+    </section>
+    <script
+      src="https://code.jquery.com/jquery-3.6.4.slim.min.js"
+      integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw="
+      crossorigin="anonymous"
+    ></script>
+    <script src="owl/owl.carousel.min.js"></script>
+    <script src="owl/script.js"></script>
+  </body>
+</html>
+
+<?php
+
+include("baglanti.php");
+
+if(isset($_POST["isim"], $_POST["tel"] , $_POST["mail"] , $_POST["konu"] , $_POST["mesaj"]))
+{
+$adsoyad=$_POST["isim"];
+$telefon=$_POST["tel"];
+$email=$_POST["mail"];
+$konu=$_POST["konu"];
+$mesaj=$_POST["mesaj"];
+
+$ekle="INSERT INTO iletisim( adsoyad, telefon, email, konu, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+if($baglan->query($ekle)===TRUE){ 
+  echo "
+<script>
+  alert('mesajınız başarı ile gönderilmiştir.');
+</script>"; } else{ echo"
+<script>
+  alert('Mesajınız gönderilirken hata oluştu.'
+</script>
+"; } } 
+?>
